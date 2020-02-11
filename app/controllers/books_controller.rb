6c3cfd52067ba16c91b592eab26381e3ad24905a
Ubuntu <ubuntu@ip-172-31-15-148.us-east-2.compute.internal>
2020-02-11 01:16:50 +0000

    Creacion de primeras vistas de books

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,37 @@
+class BooksController < ApplicationController
+    
+    def index
+        
+    end
+    
+    def show
+        
+    end
+    
+    def new
+    
+    end
+    
+    def create
+    
+    end
+    
+    def edit
+    
+    end
+    
+    def update
+    
+    end
+    
+    def destroy
+        
+    end
+    
+    private
+    
+    def book_params
+        
+    end
+    
+end
