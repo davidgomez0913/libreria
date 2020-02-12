f6f744a64c6c9a8b95fa015965517afdae8198b1
Ubuntu <ubuntu@ip-172-31-15-148.us-east-2.compute.internal>
2020-02-12 01:56:13 +0000

    Se creo el crud de los libros.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,37 +1,41 @@
 class BooksController < ApplicationController
     
     def index
-        
+        @books=Book.all
     end
     
     def show
-        
+        @book=Book.find(params[:id])
     end
     
     def new
-    
+        @book=Book.new()
     end
     
     def create
-    
+        book = Book.create(book_params)
+        redirect_to book
     end
     
     def edit
-    
+        @book=Book.find(params[:id])
     end
     
     def update
-    
+        book = Book.find(params["id"])
+        book.update(book_params)
+        redirect_to book
     end
     
     def destroy
-        
+        Book.find(params["id"]).destroy
+        redirect_to "/books"
     end
     
     private
     
     def book_params
-        
+        params.require(:book).permit(:name_book, :author, :date_published, :gender, :link_download, :image)
     end
     
 end
